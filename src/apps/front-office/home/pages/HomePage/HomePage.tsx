5b908b5aa37fab10934600cb3109e1388180c46d
unknown <sallathemess@gmail.com>
2024-07-23 17:57:42 +0300

    Update

---
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { trans } from "@mongez/localization";
+import Helmet from "@mongez/react-helmet";
+import { Button } from "apps/front-office/design-system/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <>
+      <Helmet title={trans("home")} appendAppName={false} />
+      <Button className="bg-primary-default hover:bg-primary-dark font-custom">Home</Button>
+    </>
+  );
+}
