4ac20cff2bfc7dd1cf10481d61d51edb9a1c927c
hassanzohdy <hassanzohdy@gmail.com>
2024-07-24 18:29:15 +0300

    Added welcome text

--- src/apps/front-office/home/pages/HomePage/HomePage.tsx
+++ src/apps/front-office/home/pages/HomePage/HomePage.tsx
@@ -2,6 +2,8 @@ import { trans } from "@mongez/localization";
 import Helmet from "@mongez/react-helmet";
 import { Button } from "apps/front-office/design-system/components/ui/button";
 
+//
+
 export default function HomePage() {
   return (
     <>
