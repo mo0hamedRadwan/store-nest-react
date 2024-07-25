f89b7ad516a37d6cb69525aadc680ba31428f4c8
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-07-26 01:57:37 +0300

    feat: add FeaturedCategorie banners

---
+++ src/apps/front-office/home/components/FeaturedCategorieBanner/FeaturedCategoriesBanner.tsx
@@ -0,0 +1,26 @@
+import { Button } from 'apps/front-office/design-system/components/ui/button';
+
+interface FeaturedCategoryCardProps {
+    backgroundImage: string;
+    title: string;
+}
+
+const FeaturedCategoriesBanner = ({ backgroundImage, title }: FeaturedCategoryCardProps) => {
+    return (
+        <div
+            className="relative h-[65vh] max-h-[35vh] rounded-lg shadow-md p-6 m-4 w-full sm:w-1/2 lg:w-1/3"
+            style={{ background: `url(${backgroundImage}) no-repeat center center / cover` }}
+        >
+            <div className="absolute z-[2] top-1/2 left-[40%] transform -translate-x-1/2 -translate-y-1/2 py-0">
+                <h4 className="font-bold min-h-[100px] mb-[15px]" style={{ whiteSpace: 'pre-line' }}>
+                    {title}
+                </h4>
+                <Button className="bg-primary-default hover:bg-primary-dark font-custom leading-[1.3] pl-3 pr-2 py-[7px] text-sm h-9">
+                    shop now
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedCategoriesBanner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      spacing: {
+        "1/11":"9.0909090909%"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
