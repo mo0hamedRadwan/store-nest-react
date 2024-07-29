d4208b0bfba00a4efa6ab01cca9e1a47b86e1973
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-07-30 00:22:44 +0300

    fix: issue of responsive

--- src/apps/front-office/home/pages/HomePage/sections/FeaturedCategories/FeaturedCategories.tsx
+++ src/apps/front-office/home/pages/HomePage/sections/FeaturedCategories/FeaturedCategories.tsx
@@ -41,7 +41,7 @@ export default function FeaturedCategories() {
         <section className="featured-categories px-3 py-[25px]">
 
             <div className="featured-categories-title relative mb-11 ">
-                <h3 className="text-3xl lg:text-4xl font-bold text-[#253D4E]">Featured Categories</h3>
+                <h3 className="sm:text-2xl lg:text-4xl font-bold text-[#253D4E]">Featured Categories</h3>
                 {/* <div className="flex relative max-w-[200px] justify-end top-[unset] z-[2] w-full">
                 <span className="relative w-10 h-10 leading-[44px] text-2xl cursor-pointer block z-[100] transition-all duration-[0.2s] ease-[ease-out] text-center text-[#7E7E7E] rounded-[50%] inset-x-[unset] bg-[#f2f3f4] mr-2.5 " >L </span>
                 <span className="relative w-10 h-10 leading-[44px] text-2xl cursor-pointer block z-[100] transition-all duration-[0.2s] ease-[ease-out] text-center text-[#7E7E7E] rounded-[50%] inset-x-[unset] bg-[#f2f3f4]" >R</span>
@@ -55,9 +55,9 @@ export default function FeaturedCategories() {
                     onMouseEnter={plugin.current.stop}
                     onMouseLeave={plugin.current.reset}
                 >
-                    <CarouselContent className="w-full overflow-visible">
+                    <CarouselContent className="w-full overflow">
                         {Array.from({ length: 15 }).map((_, index) => (
-                            <CarouselItem key={index} className="pl-1 basis-1/5 lg:basis-1/11 md:basis-1/5 me-2"
+                            <CarouselItem key={index} className="pl-1 basis-1/4 sm:basis-1/4 md:basis-1/5 lg:basis-1/11  me-2"
                             >
                                 <div className="p-1">
                                     <Card style={{ backgroundColor: backgroundColors[index % backgroundColors.length] }}>
@@ -68,7 +68,7 @@ export default function FeaturedCategories() {
                                                         src="public/images/featured-categories/kiwi.png" alt="img-product" />
                                                 </a>
                                             </figure>
-                                            <h6><a className="font-bold">Oganic Kiwi</a></h6>
+                                            <p><a className=" text-slate-800 font-bold leading-relaxed md:text-lg">Oganic Kiwi</a></p>
                                             <span className="text-slate-400 text-base font-normal my-1">28 items</span>
                                         </CardContent>
                                     </Card>
@@ -76,8 +76,10 @@ export default function FeaturedCategories() {
                             </CarouselItem>
                         ))}
                     </CarouselContent>
-                    <CarouselPrevious className="-top-1/3 left-[90%] text-[#7E7E7E] bg-[#f2f3f4] hover:bg-primary-dark hover:text-[#f2f3f4] font-custom" />
-                    <CarouselNext className="-top-1/3 left-[93%] text-[#7E7E7E] bg-[#f2f3f4] hover:bg-primary-dark hover:text-[#f2f3f4] font-custom" />
+                    <div className="flex relative justify-end top-[-120.333333%] z-[2] w-full">
+                        <CarouselPrevious className="w-4 h-4 lg:w-8 lg:h-8 md:w-6 md:h-6 sm:w-5 sm:h-5  left-[94%] text-[#7E7E7E] bg-[#f2f3f4] hover:bg-primary-dark hover:text-[#f2f3f4] font-custom" />
+                        <CarouselNext className="w-4 h-4 lg:w-8 lg:h-8 md:w-6 md:h-6 sm:w-5 sm:h-5 ml-2 left-[97%] text-[#7E7E7E] bg-[#f2f3f4] hover:bg-primary-dark hover:text-[#f2f3f4] font-custom" />
+                    </div>
                 </Carousel>
             </div>
 
@@ -86,7 +88,6 @@ export default function FeaturedCategories() {
                     <FeaturedCategoriesBanner
                         key={index}
                         backgroundImage={card.backgroundImage}
-                        title={card.title}
                     />
                 ))}
             </div>
