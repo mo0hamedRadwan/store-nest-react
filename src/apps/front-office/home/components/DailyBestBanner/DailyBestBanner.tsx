2998604659ae9daddc9d611959c20d9e55cf357c
Karim-Muhammad <kimoomar007@gmail.com>
2024-07-31 00:57:53 +0300

    ref:reduce some lines in Banner Component

--- src/apps/front-office/home/components/DailyBestBanner/DailyBestBanner.tsx
+++ src/apps/front-office/home/components/DailyBestBanner/DailyBestBanner.tsx
@@ -8,16 +8,14 @@ export default function DailyBestBanner() {
         backgroundImage: `url(/public/images/daily-best-products/banner-4.png)`,
       }}>
       <div className="daily-best-banner__content absolute top-0 left-0 p-12">
-        <div className="banner-text text-4xl font-bold mb-32 text-black-soft-foreground">
+        <h3 className="banner-text text-4xl font-bold mb-32 text-black-soft-foreground">
           Bring nature into your home
-        </div>
+        </h3>
 
-        <div className="banner-btn">
-          <button className="font-medium rounded-sm bg-primary hover:bg-[#FDC040] p-2 text-white text-sm flex items-center gap-2 group transition-all">
-            Shop Now{" "}
-            <ArrowRightFromLine className="block w-4 h-4 group-hover:ms-2 transition-all" />
-          </button>
-        </div>
+        <button className="font-medium rounded-sm bg-primary hover:bg-[#FDC040] p-2 text-white text-sm flex items-center gap-2 group transition-all">
+          Shop Now{" "}
+          <ArrowRightFromLine className="block w-4 h-4 group-hover:ms-2 transition-all" />
+        </button>
       </div>
     </div>
   );
