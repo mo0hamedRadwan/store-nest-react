a431ce0d5e5ab536a34524ab86b6a06d30cc02d2
Abdelrady <ahmedmc95@gmail.com.com>
2024-08-06 17:42:46 +0300

    fix:style

--- src/apps/front-office/shop/pages/ShopPage/ShopPage.tsx
+++ src/apps/front-office/shop/pages/ShopPage/ShopPage.tsx
@@ -1,7 +1,6 @@
-import { trans } from "@mongez/localization";
-import Breadcrumb from "apps/front-office/design-system/layouts/Breadcrumb";
 import ProductsContainer from "../components/ProductsContainer";
 import ViewDisplayMode from "../components/ProductsContainer/ViewDisplayMode";
+import ShopBanner from "../components/shopBanner/ShopBanner";
 
 export type ShopPageProps = {
   // props go here
@@ -9,7 +8,7 @@ export type ShopPageProps = {
 function _ShopPage() {
   return (
     <>
-      <Breadcrumb title={trans("shop")} navItems={[{ name: trans("shop") }]} />
+      <ShopBanner />
       <div className="container">
         <div className="md:flex md:flex-row md:justify-between md:gap-12 md:my-12">
           <div className="md:basis-1/4 md:mt-7 md:visible md:block md:z-0 z-[60] top-0 h-screen fixed md:relative rtl:right-0 ltr:left-0 w-10/12 sm:w-1/2 md:w-1/3 bg-white p-[10px] transition-all">
