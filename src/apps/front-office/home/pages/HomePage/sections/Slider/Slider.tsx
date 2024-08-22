4502ade0f949211704e6c6de9daa5ba26759bd77
islamabozeed <islammahmoud13@gmail.com>
2024-08-22 17:41:07 +0300

    fix pagination position and arrow color

--- src/apps/front-office/home/pages/HomePage/sections/Slider/Slider.tsx
+++ src/apps/front-office/home/pages/HomePage/sections/Slider/Slider.tsx
@@ -3,7 +3,8 @@ import { useSlider } from "shared/hooks/use-slider";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css/effect-fade";
+import { Autoplay, Navigation, Pagination , EffectFade  } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./slider.css";
 
@@ -14,8 +15,11 @@ export default function Slider() {
   return (
     <div className="relative group">
       <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
+        modules={[Navigation, Pagination, Autoplay, EffectFade]}
         loop={true}
+        effect="fade"
+        virtualTranslate
+
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
@@ -43,7 +47,7 @@ export default function Slider() {
             </div>
           </SwiperSlide>
         ))}
-        <div className="swiper-pagination absolute !bottom-5 left-1/2  transform -translate-x-1/2 z-10"></div>
+        <div className="swiper-pagination absolute !bottom-8 left-1/2  transform -translate-x-1/2 z-10"></div>
       </Swiper>
       <div className="swiper-button-prev absolute top-1/2 !left-7 z-10 w-10 h-10 rounded-full bg-white opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 hover:bg-primary-default"></div>
       <div className="swiper-button-next absolute top-1/2 !right-7 z-10 w-10 h-10 rounded-full bg-white opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 hover:bg-primary-default"></div>
