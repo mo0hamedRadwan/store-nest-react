1058e71d6e6dc3c0dd6f490bf3f2c51f35d23cb0
Karim-Muhammad <kimoomar007@gmail.com>
2024-09-12 21:31:32 +0300

    add third part (our performance section

--- src/apps/front-office/informative-pages/pages/AboutUsPage/AboutUsPage.tsx
+++ src/apps/front-office/informative-pages/pages/AboutUsPage/AboutUsPage.tsx
@@ -13,6 +13,7 @@ import {
 } from "src/apps/front-office/utils/helpers";
 
 import provide_mock from "../../mocks/we-provide.mock";
+import HeaderPage from "./components/HeaderPage";
 
 export default function AboutUsPage() {
   return (
@@ -89,9 +90,7 @@ export default function AboutUsPage() {
           </div>
 
           <div className="body-t-2">
-            <h2 className="text-4xl font-bold text-center capitalize my-4 bg-[url('https://nest-frontend-rtl-v6.vercel.app/assets/imgs/theme/wave.png')] bg-no-repeat bg-bottom pb-7">
-              What we provide?
-            </h2>
+            <HeaderPage title="What we provide" />
 
             <section className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex-wrap gap-7 py-10">
               {provide_mock.map((item, index) => (
@@ -106,7 +105,35 @@ export default function AboutUsPage() {
               ))}
             </section>
           </div>
-          <div className="body-t-3"></div>
+
+          <div className="body-t-3">
+            {/* <HeaderPage title="Our Team" /> */}
+            <div className="container grid grid-cols-1 md:grid-cols-2">
+              <div className="image-r">
+                <img src="https://nest-frontend-rtl-v6.vercel.app/assets/imgs/page/about-5.png" />
+              </div>
+              <div className="info-l text-right">
+                <h4 className="text-2xl capitalize text-gray-400 my-4">
+                  our performance
+                </h4>
+
+                <h2 className="text-5xl font-bold my-4">
+                  Your Partner for e-commerce grocery solution
+                </h2>
+
+                <p className="text-muted-foreground leading-7">
+                  Ed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo inventore veritatis et quasi architecto
+                  <p className="my-4">
+                    Pitatis et quasi architecto beatae vitae dicta sunt
+                    explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+                    aspernatur aut odit aut fugit, sed quia
+                  </p>
+                </p>
+              </div>
+            </div>
+          </div>
           <div className="body-t-4"></div>
           <div className="body-t-5"></div>
           <div className="body-t-6"></div>
