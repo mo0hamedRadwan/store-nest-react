1a6b08f315f05f5ee88eac434886b4399aa53b9f
hassanzohdy <hassanzohdy@gmail.com>
2024-09-13 17:36:03 +0300

    Resolving conflicts

---
+++ src/apps/front-office/catalog/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,10 @@
+export type ProductCardProps = {
+  // props go here
+};
+export default function ProductCard(props: ProductCardProps) {
+  return (
+    <>
+      <h1>ProductCard</h1>
+    </>
+  );
+}
