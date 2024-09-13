442a83802576b1e7d47ec5310972b77106d603e5
islamabozeed <islammahmoud13@gmail.com>
2024-09-13 17:23:56 +0300

    privacy page

--- src/apps/front-office/pages-info/routes.ts
+++ src/apps/front-office/pages-info/routes.ts
@@ -1,10 +1,15 @@
-import URLS from "apps/front-office/utils/urls";
 import { publicRoutes } from "apps/front-office/utils/router";
+import URLS from "apps/front-office/utils/urls";
 import PagesInfoPage from "./pages/PagesInfoPage";
+import PrivacyPage from "./pages/privacyPage";
 
 publicRoutes([
   {
     path: URLS.pagesInfo,
     component: PagesInfoPage,
+  },
+  {
+    path: URLS.pages.privacyPolicy,
+    component: PrivacyPage,
   },
 ]);
