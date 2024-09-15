bb3fb925381e62f1291926ddeb24be2b9a671aac
mo0hamedRadwan <mo0hamedradwan@yahoo.com>
2024-09-15 16:00:30 +0300

    test

--- src/apps/front-office/home/pages/HomePage/sections/DealsDayTwo/TopSelling.tsx
+++ src/apps/front-office/home/pages/HomePage/sections/DealsDayTwo/TopSelling.tsx
@@ -1,34 +1,40 @@
 import TopSellingCard from "apps/front-office/design-system/components/TopSelling/TopSellingCard";
 import TopSellingHead from "apps/front-office/design-system/components/TopSelling/TopSellingHead";
-import { useEffect, useState } from "react";
 import { Product } from "src/apps/front-office/shop/utils/types";
-import { getTopsellingHttpsList } from "./topselling-http-service";
 
-export default function TopSelling({ moduleName }) {
-  const [productData, setProductData] = useState<Product | any>([]);
+type TopSellingPropsType = {
+  moduleName: string;
+  products: Product[];
+};
+
+export default function TopSelling({
+  moduleName,
+  products,
+}: TopSellingPropsType) {
+  // const [productData, setProductData] = useState<Product | any>([]);
   // const [productTitle, setProductTitle] = useState<string | any>({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await getTopsellingHttpsList();
-        for (const row of res.rows) {
-          for (const column of row.columns) {
-            if (column.module.name === moduleName) {
-              const data = column.module.products;
-              // const title = column.module.title;
-              setProductData(data);
-              // setProductTitle(title);
-              return;
-            }
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching ", error);
-      }
-    };
-    fetchData();
-  }, [moduleName]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await getTopsellingHttpsList();
+  //       for (const row of res.rows) {
+  //         for (const column of row.columns) {
+  //           if (column.module.name === moduleName) {
+  //             const data = column.module.products;
+  //             // const title = column.module.title;
+  //             setProductData(data);
+  //             // setProductTitle(title);
+  //             return;
+  //           }
+  //         }
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching ", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [moduleName]);
 
   return (
     <div>
@@ -36,12 +42,12 @@ export default function TopSelling({ moduleName }) {
         <TopSellingHead productTitle={moduleName} />
       </div>
       <div>
-        {productData.map(el => {
+        {products.map(el => {
           return (
             <div key={el.id}>
               <TopSellingCard
                 productImageTop={el.images[0].url}
-                productDescription={el.shortDescription}
+                productName={el.name}
                 salePrice={el.salePrice}
                 priceOld={el.price}
               />
--- src/apps/front-office/home/utils/flags.ts
+++ src/apps/front-office/home/utils/flags.ts
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import SliderModule from "../components/SliderModule";
+import TopProductsModule from "../components/TopProductsModule";
 import FeaturedCategories from "../pages/HomePage/sections/FeaturedCategories";
 import { Module } from "./types";
 
@@ -8,4 +9,5 @@ type ModulesListMap = Record<string, FunctionComponent<{ module: Module }>>;
 export const modulesMap: ModulesListMap = {
   slider: SliderModule,
   categories: FeaturedCategories,
+  products: TopProductsModule,
 };
