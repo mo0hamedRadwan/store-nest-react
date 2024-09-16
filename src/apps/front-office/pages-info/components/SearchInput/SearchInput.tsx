5ba2b75a0a0c928e00746e68b3f3d78677f31843
DanaMedany <DanaMedany94@gmail.com>
2024-09-16 21:29:35 +0400

    update Term and condition page with Lato font, color, and size

--- src/apps/front-office/pages-info/components/SearchInput/SearchInput.tsx
+++ src/apps/front-office/pages-info/components/SearchInput/SearchInput.tsx
@@ -4,12 +4,16 @@ export default function SearchInput() {
   return (
     <div className="relative mb-9">
       <input
-        className="border border-solid border-[#ececec] py-2 w-full pl-4 text-[16px] pr-10 rounded-[10px] h-16 outline-none focus:border-shopCategoryColor "
+        className="border border-solid border-[#ececec] py-2 w-full pl-4 text-[16px] pr-16 rounded-[10px] h-16 outline-none focus:border-shopCategoryColor font-lato"
         type="text"
         name="Search"
         placeholder={trans("search")}
       />
-      <i className="bx bx-search text-xl absolute left-[80%] top-1/2 transform -translate-y-1/2 text-gray-500"></i>
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 text-[23px] h-full px-6 bg-transparent text-[#242424] border-none rounded-[10px] hover:bg-priceNowColor hover:text-white transition-all duration-300 ease-in-out"
+        type="submit">
+        <i className="bx bx-search"></i>
+      </button>
     </div>
   );
 }
